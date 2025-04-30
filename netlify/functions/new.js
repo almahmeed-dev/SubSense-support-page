0216910c8827e1fa7351633c186d2cdfb2244f65
almahmeed-dev <156545322+almahmeed-dev@users.noreply.github.com>
2025-04-30 10:02:17 +0300

    Create new.js

---
+++ netlify/functions/new.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto')
+const { Pool } = require('pg')
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+
+exports.handler = async (event) => {
+  // TODO: authenticate inviter (e.g. via a JWT header)
+  const inviterId = /* extract from auth */  
+  const token = crypto.randomBytes(16).toString('hex')
+  await pool.query(
+    'INSERT INTO referral_tokens(token, inviter_user_id, status) VALUES($1,$2,$3)',
+    [token, inviterId, 'new']
+  )
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      token,
+      url: `https://${process.env.APP_DOMAIN}/invite?token=${token}`
+    })
+  }
+}
